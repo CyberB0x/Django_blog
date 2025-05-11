9f749aede057b764a93325b256ab93f41d34a28c
Arslon <arslonbekefsun09@gmail.com>
2025-05-11 22:23:26 +0300

    Django blog

---
+++ myblog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post/<int:post_id>', views.post_detail, name='post_detail'),
+    path('create/', views.create_post, name='create_post'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+]
